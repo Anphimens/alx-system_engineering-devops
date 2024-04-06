20b61e75a994e4dd9fbd6408e376181f7ce042eb
Anphimens <philipinamensah19@gmail.com>
2024-04-06 00:29:18 -0700

    Puppet file to create a custom HTTP header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,4 @@
-# Installs a Nginx server with custome HTTP header
+# Installs a Nginx server with custom HTTP header
 
 exec {'update':
   provider => shell,
@@ -7,20 +7,19 @@ exec {'update':
 }
 
 exec {'install Nginx':
-  provider  => shell,
-  command   => 'sudo apt-get -y install nginx',
-  before    => Exec['add_header'],
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
 exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
 exec { 'restart Nginx':
-  provider  => shell,
-  command   => 'sudo service nginx restart',
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
-
